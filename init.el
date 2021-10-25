a139231fe4dcf7b86608ef4ea5383ca6a94d64a1
MisterJaJo <jarvistrigo@gmail.com>
2021-10-25 22:00:09 +0200

    Got really frustrated trying to setup LSP with Haskell

--- init.el
+++ init.el
@@ -79,8 +79,6 @@
 ; Programming languages + editor integration
 (use-package rust-mode)
 
-(use-package haskell-mode)
-
 (use-package cmake-mode)
 
 (use-package editorconfig
@@ -299,7 +297,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(haskell-mode cmake-mode doom-themes all-the-icons treemacs doom-modeline company company-box helm rust-mode editorconfig lsp-mode lsp-ui lsp-treemacs helm-lsp flycheck flycheck-rust hydra which-key projectile helm-xref)))
+   '(cmake-mode doom-themes all-the-icons treemacs doom-modeline company company-box helm rust-mode editorconfig lsp-mode lsp-ui lsp-treemacs helm-lsp flycheck flycheck-rust hydra which-key projectile helm-xref)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
