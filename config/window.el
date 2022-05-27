345964a5ded519848eddbfea5567c811269ef499
MisterJaJo <jarvistrigo@gmail.com>
2022-05-27 17:47:59 +0200

    Split up keybinds.el configuration file

---
+++ config/window.el
@@ -0,0 +1,11 @@
+;;; window.el -- Configuration of window related features -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+
+;; Rebind C-x C-b to open ibuffer instead of buffer-list
+;; Use this for managing (closing etc.) buffers
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(provide 'window)
+;;; window.el ends here
